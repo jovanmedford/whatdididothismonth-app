ef154c7b01e99bae7fe833fdfb153c6514cab775
Jovan <jovanmedford@gmail.com>
2026-05-09 10:19:17 -0400

    feat: improve error page

--- app/error/page.tsx
+++ app/error/page.tsx
@@ -1,5 +1,17 @@
+
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: "Error",
+}
+
 export default function ErrorPage() {
     return (
-        <h1>An Error occurred.</h1>
+        <div className="flex flex-col items-center  min-h-screen">
+            <div className="mt-36">
+                <h1 className="text-3xl font-bold">Error occurred.</h1>
+                <p className="text-gray-600">An unexpected error has occurred. Please try again later.</p>
+            </div>
+        </div>
     )
 }
