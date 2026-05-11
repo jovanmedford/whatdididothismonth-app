d6653ef056fd198a0b25666f8c88496ba5a3d038
Jovan <jovanmedford@gmail.com>
2026-05-11 19:41:25 -0400

    feat: add name label to verify signed in user

--- app/_components/app-header/app-header.tsx
+++ app/_components/app-header/app-header.tsx
@@ -1,15 +1,17 @@
 import { Logo } from "../logo";
 import Link from "next/link";
 import LogoutButton from "./logout-button";
+import { verifySession } from "@/lib/dal";
 
-export function AppHeader() {
+export async function AppHeader() {
+    const { user } = await verifySession();
     return (
         <header className="flex justify-between items-center mb-10">
             <Link href="/calendar">
                 <Logo />
             </Link>
             <div className="flex gap-2 items-center">
-                <span>John Doe</span>
+                <span>{user.name}</span>
                 <LogoutButton />
             </div>
         </header>
